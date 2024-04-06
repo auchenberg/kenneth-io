7de56f6dd5f39c489447890ebbd9e630ff758d79
Kenneth Auchenberg <kenneth@auchenberg.dk>
2024-04-06 19:02:46 -0400

    Fix favicon

--- components/head.js
+++ components/head.js
@@ -6,7 +6,7 @@ const Head = (props) => (
     <title>{props.title}</title>
     <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/static/favicon.ico" />
+    <link rel="icon" href="/favicon.ico" />
     <link
       rel="stylesheet"
       href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
