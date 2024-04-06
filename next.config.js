ed8b3e967710914cfc4c5a0de848191732dd25b7
Kenneth Auchenberg <kenneth@auchenberg.dk>
2024-04-06 18:28:22 -0400

    Update build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+
   webpack: (config) => {
-    config.resolve.fallback = {fs: false};
+    config.resolve.fallback = { fs: false };
 
     return config;
   },
-};
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+
+module.exports = nextConfig
+
