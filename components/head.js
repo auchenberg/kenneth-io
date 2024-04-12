b36f7b2cc9e574bb1fc572f3935c6bfe9e5226d1
Kenneth Auchenberg <kenneth@auchenberg.dk>
2024-04-12 14:55:13 -0400

    Get rid of xrp

--- components/head.js
+++ components/head.js
@@ -13,11 +13,6 @@ const Head = (props) => (
       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
       crossOrigin="anonymous"
     />
-
-    <meta
-      name="monetization"
-      content="$coil.xrptipbot.com/KpgTb2m4QWeVNe6HXKzodg"
-    />
   </NextHead>
 );
 
