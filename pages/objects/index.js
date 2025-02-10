574822878e709e83972e3186f242553763bb8e73
Kenneth Auchenberg <kenneth@auchenberg.dk>
2025-02-09 21:20:46 -0500

    Add todos

--- pages/objects/index.js
+++ pages/objects/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import Layout from '../../components/layout';
 import Link from 'next/link';
 import Image from 'next/image';
-// Inspiration: https://paulstamatiou.com/
+
+
+// TODO:
+// - Bookcases
+// - Gaggennau
 
 
 const ObjectsPage = () => {
